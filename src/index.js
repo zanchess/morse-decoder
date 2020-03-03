19fbc5dd7bff7e102bdc2a466a35c041cbc40992
Alexander <46690347+zanchess@users.noreply.github.com>
2020-03-04 00:41:37 +0300

    feat: end morse decoded task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     /*Первая часть работает с предложениями, вторая с цельной строкой*/
+		let arr3 =[]
+    if (expr.indexOf('**********') != -1 ) {//проверка на наличие пробелов
+			arr = expr.split('**********');//разделить закодированные слова
+    	let allExpr = [];//массив из будущих слов предложения
+			for (let i = 0; i < arr.length; i++) {
+        let arr2 = arr[i].match(/.{1,10}/g); //разделить код по 10 симолов согласно условию (10 симоволов - 1буква)
+        let word = ""; //будущее слово входящее в предложение
+				for (let j = 0; j < arr2.length; j++) {
+					arr3 = arr2[j].match(/.{1,2}/g);//разделение шифра на массив из пар цифр
+					let key = ""; //будущий ключ символа
+					for (let k = 0; k < arr3.length; k++) { //составление ключа
+						if (arr3[k] === "10") {
+							key += ".";
+						} else if (arr3[k] === "11") {
+							key += "-";
+						}
+					}
+					if (MORSE_TABLE.hasOwnProperty(key)) { //составление слова
+						word +=`${MORSE_TABLE[key]}`;
+					}
+				}
+        allExpr.push(word); //составление массива из слов предложения
+			}
+      return allExpr.join(' '); //возврат строки
+		} else{
+        let arr2 = expr.match(/.{1,10}/g);
+        let word = "";
+				for (let j = 0; j < arr2.length; j++) {
+					let arr3 = arr2[j].match(/.{1,2}/g);
+					let key = "";
+					for (let k = 0; k < arr3.length; k++) {
+						if (arr3[k] === "10") {
+							key += ".";
+						} else if (arr3[k] === "11") {
+							key += "-";
+						}
+					}
+					if (MORSE_TABLE.hasOwnProperty(key)) {
+						word +=`${MORSE_TABLE[key]}`;
+					}
+        }
+        return word; 
+    }
 }
 
 module.exports = {
